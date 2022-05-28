85a8934e8581dbdff5d6a2e2b9a1dbda998c849d
taviyaumesh007 <88816691+taviyaumesh007@users.noreply.github.com>
2022-05-29 00:11:19 +0530

    product catch from mongodb

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRouter.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI).then(() => {
@@ -11,28 +13,9 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 })
 
 const app = express();
-// test
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-});
-app.get('/api/products/slug/:slug', (req, res) => {
-    const product = data.products.find(x => x.slug === req.params.slug);
-    if (product) {
-        res.send(product)
-    }
-    else {
-        res.status(404).send({ message: 'Product not found' })
-    }
-});
-app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find((x) => x._id === req.params.id);
-    if (product) {
-        res.send(product)
-    }
-    else {
-        res.status(404).send({ message: 'Product not found' })
-    }
-});
+app.use('/api/seed',seedRouter)
+app.use('/api/products',productRouter)
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`);
