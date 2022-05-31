1d57b633e176769ea4f55dd6a70440b3dfc5ccd1
taviyaumesh007 <88816691+taviyaumesh007@users.noreply.github.com>
2022-05-31 11:09:29 +0530

    create backed api for Signin

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRouter.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URI).then(() => {
@@ -13,8 +14,16 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 })
 
 const app = express();
-app.use('/api/seed',seedRouter)
-app.use('/api/products',productRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/seed', seedRouter)
+app.use('/api/products', productRouter)
+app.use('/api/users', userRouter)
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message })
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
